760b4481b20c7264426f70dc665802fd7733af76
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2015-06-11 09:42:11 +0300

    show some data

--- app/ordermonitor.controller.js
+++ app/ordermonitor.controller.js
@@ -1,4 +1,5 @@
 (function() {
+    'use strict';
 
     angular
         .module('ordermonitor')
@@ -7,6 +8,16 @@
     function OrderMonitorController() {
 
         var viewModel = this;
+        viewModel.monitorRowsShown = 26;
+        viewModel.indexOfFirstShownRow = 0;
+
+        viewModel.rows = [];
+        var names = ['juho', 'kari', 'matias', 'chao'];
+        for (var i = 0; i < 300; i++) {
+            viewModel.rows.push({number:i, square: i*i, cube: i*i*i, name: names[i%names.length]});
+        }
+        viewModel.selectedRowIndex = 0;
+        viewModel.selectedNumber = viewModel.rows[viewModel.selectedRowIndex];
 
     }
 
